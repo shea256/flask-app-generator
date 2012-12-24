2d13f1fe46ecf2130d9a6943f88dbf3cd2e7e8d9
rxl <me@ryanshea.org>
2012-12-24 17:02:17 -0500

    code organization

--- generate.py
+++ generate.py
@@ -13,9 +13,133 @@ def git_add_and_commit(message):
 	os.system('git add .')
 	os.system('git commit -m "' + message + '"')
 
-def usage():
-	print "usage: python build_app.py <app_name>"
-	print "options: '--githubuser=<user_name>', '--githubrepo=<repo_name>', '--herokuapp=<app_name>'"
+def create_app_files(app_name):
+	#----------------------------------------
+	# create directory
+	#----------------------------------------
+	os.mkdir(app_name)
+	os.chdir(app_name)
+
+	#----------------------------------------
+	# git files
+	#----------------------------------------
+	os.system('git init')
+	os.system('touch README.md')
+
+	with open('.gitignore', 'w') as f:
+		f.write(render_filedata('gitignore'))
+
+	#----------------------------------------
+	# heroku files
+	#----------------------------------------
+	with open('Procfile', 'w') as f:
+		f.write(render_filedata('procfile'))
+
+	#----------------------------------------
+	# python files
+	#----------------------------------------
+	with open('app.py', 'w') as f:
+		f.write(render_filedata('main_app'))
+
+	#----------------------------------------
+	# templates
+	#----------------------------------------
+	os.mkdir('templates')
+
+	with open('templates/base.html', 'w') as f:
+		f.write(render_filedata('base_template', app_name=app_name))
+
+	with open('templates/index.html', 'w') as f:
+		f.write(render_filedata('index_template', app_name=app_name))
+
+	with open('templates/404.html', 'w') as f:
+		f.write(render_filedata('404_template'))
+
+	#----------------------------------------
+	# static files
+	#----------------------------------------
+	os.mkdir('static')
+	os.makedirs('static/css')
+	os.makedirs('static/js')
+	os.makedirs('static/img')
+	os.makedirs('static/ico')
+
+	os.system('curl ' + bootstrapcss_url + ' > static/css/bootstrap.css')
+	os.system('curl ' + bootstrapjs_url + ' > static/js/bootstrap.js')
+	os.system('curl ' + bootstrapjsmin_url + ' > static/js/bootstrap.min.js')
+	os.system('curl ' + jquery_url + ' > static/js/jquery.js')
+	os.system('curl ' + jquerymin_url + ' > static/js/jquery.min.js')
+	os.system('curl ' + favicon_url + ' > static/ico/favicon.ico')
+
+def configure_github(github_user, github_repo):
+	os.system('git remote add origin https://github.com/' + github_user + '/' + github_repo + '.git')
+
+def configure_heroku(heroku_app):
+	os.system('heroku create --stack cedar')
+	os.system('heroku apps:rename ' + heroku_app)
+
+def configure_virtualenvwrapper(app_name, virtualenvwrapper_path):
+	os.system('source ' + virtualenvwrapper_path + '; mkvirtualenv ' + app_name + '; workon ' + app_name + '; sudo pip install Flask; pip freeze > requirements.txt')
+
+def configure_virtualenv(app_name):
+	os.system('virtualenv venv --distribute; source venv/bin/activate; sudo pip install Flask; pip freeze > requirements.txt')
+
+def main():
+	#----------------------------------------
+	# parse arguments
+	#----------------------------------------
+	parser = argparse.ArgumentParser(description='Autogenerate a Flask app')
+	parser.add_argument('appname', help='the name of the app to be created')
+	parser.add_argument('--githubuser', dest='github_user', help='the name of the github user that will be hosting the app')
+	parser.add_argument('--githubrepo', dest='github_repo', help='the name of the remote github repo that will be hosting the app')
+	parser.add_argument('--herokuapp', dest='heroku_app', help='the name that the heroku app will be renamed to if it has not yet been taken')
+	parser.add_argument('--venvname', dest='virtualenv_name', help='the name of the virtualenv for the app')
+	parser.add_argument('--venvwrapperpath', dest='virtualenvwrapper_path', help='the path to the file virtualenvwrapper.sh (turns on virtualenvwrapper when specified)')
+	parser.add_argument('--push', dest='push', help='automatically push to github and heroku when app has been created', action='store_true')
+	args = parser.parse_args()
+
+	#----------------------------------------
+	# create app files
+	#----------------------------------------
+	create_app_files(args.appname)
+
+	#----------------------------------------
+	# configure github
+	#----------------------------------------
+	if args.github_user and args.github_repo:
+		configure_github(args.github_user, args.github_repo)
+
+	#----------------------------------------
+	# configure heroku
+	#----------------------------------------
+	if args.heroku_app:
+		configure_heroku(args.heroku_app)
+
+	#----------------------------------------
+	# configure virtualenv
+	#----------------------------------------
+	if args.virtualenv_name:
+		if args.virtualenvwrapper_path:
+			configure_virtualenvwrapper(args.virtualenv_name, args.virtualenvwrapper_path)
+		else:
+			configure_virtualenv(args.virtualenv_name)
+
+	#----------------------------------------
+	# commit all files
+	#----------------------------------------
+	git_add_and_commit('first commit')
+
+	#----------------------------------------
+	# push to github and heroku
+	#----------------------------------------
+	if args.push:
+		os.system('git push origin master')
+		os.system('git push heroku master; heroku open')
+
+	#----------------------------------------
+	# print success message
+	#----------------------------------------
+	print "\nYour app has been created!"
 
 def render_filedata(filename, **kwargs):
 	if filename == 'gitignore':
@@ -159,134 +283,6 @@ if __name__ == "__main__":
 {% endblock %}"""
 	else:
 		raise Exception('No template found with that name.')
-
-def create_app_files(app_name):
-	#----------------------------------------
-	# create directory
-	#----------------------------------------
-	os.mkdir(app_name)
-	os.chdir(app_name)
-
-	#----------------------------------------
-	# git files
-	#----------------------------------------
-	os.system('git init')
-	os.system('touch README.md')
-
-	with open('.gitignore', 'w') as f:
-		f.write(render_filedata('gitignore'))
-
-	#----------------------------------------
-	# heroku files
-	#----------------------------------------
-	with open('Procfile', 'w') as f:
-		f.write(render_filedata('procfile'))
-
-	#----------------------------------------
-	# python files
-	#----------------------------------------
-	with open('app.py', 'w') as f:
-		f.write(render_filedata('main_app'))
-
-	#----------------------------------------
-	# templates
-	#----------------------------------------
-	os.mkdir('templates')
-
-	with open('templates/base.html', 'w') as f:
-		f.write(render_filedata('base_template', app_name=app_name))
-
-	with open('templates/index.html', 'w') as f:
-		f.write(render_filedata('index_template', app_name=app_name))
-
-	with open('templates/404.html', 'w') as f:
-		f.write(render_filedata('404_template'))
-
-	#----------------------------------------
-	# static files
-	#----------------------------------------
-	os.mkdir('static')
-	os.makedirs('static/css')
-	os.makedirs('static/js')
-	os.makedirs('static/img')
-	os.makedirs('static/ico')
-
-	os.system('curl ' + bootstrapcss_url + ' > static/css/bootstrap.css')
-	os.system('curl ' + bootstrapjs_url + ' > static/js/bootstrap.js')
-	os.system('curl ' + bootstrapjsmin_url + ' > static/js/bootstrap.min.js')
-	os.system('curl ' + jquery_url + ' > static/js/jquery.js')
-	os.system('curl ' + jquerymin_url + ' > static/js/jquery.min.js')
-	os.system('curl ' + favicon_url + ' > static/ico/favicon.ico')
-
-def configure_github(github_user, github_repo):
-	os.system('git remote add origin https://github.com/' + github_user + '/' + github_repo + '.git')
-
-def configure_heroku(heroku_app):
-	os.system('heroku create --stack cedar')
-	os.system('heroku apps:rename ' + heroku_app)
-
-def configure_virtualenvwrapper(app_name, virtualenvwrapper_path):
-	os.system('source ' + virtualenvwrapper_path + '; mkvirtualenv ' + app_name + '; workon ' + app_name + '; sudo pip install Flask; pip freeze > requirements.txt')
-
-def configure_virtualenv(app_name):
-	os.system('virtualenv venv --distribute; source venv/bin/activate; sudo pip install Flask; pip freeze > requirements.txt')
-
-def main():
-	#----------------------------------------
-	# parse arguments
-	#----------------------------------------
-	parser = argparse.ArgumentParser(description='Autogenerate a Flask app')
-	parser.add_argument('appname', help='the name of the app to be created')
-	parser.add_argument('--githubuser', dest='github_user', help='the name of the github user that will be hosting the app')
-	parser.add_argument('--githubrepo', dest='github_repo', help='the name of the remote github repo that will be hosting the app')
-	parser.add_argument('--herokuapp', dest='heroku_app', help='the name that the heroku app will be renamed to if it has not yet been taken')
-	parser.add_argument('--venvname', dest='virtualenv_name', help='the name of the virtualenv for the app')
-	parser.add_argument('--venvwrapperpath', dest='virtualenvwrapper_path', help='the path to the file virtualenvwrapper.sh (turns on virtualenvwrapper when specified)')
-	parser.add_argument('--push', dest='push', help='automatically push to github and heroku when app has been created', action='store_true')
-	args = parser.parse_args()
-
-	#----------------------------------------
-	# create app files
-	#----------------------------------------
-	create_app_files(args.appname)
-
-	#----------------------------------------
-	# configure github
-	#----------------------------------------
-	if args.github_user and args.github_repo:
-		configure_github(args.github_user, args.github_repo)
-
-	#----------------------------------------
-	# configure heroku
-	#----------------------------------------
-	if args.heroku_app:
-		configure_heroku(args.heroku_app)
-
-	#----------------------------------------
-	# configure virtualenv
-	#----------------------------------------
-	if args.virtualenv_name:
-		if args.virtualenvwrapper_path:
-			configure_virtualenvwrapper(args.virtualenv_name, args.virtualenvwrapper_path)
-		else:
-			configure_virtualenv(args.virtualenv_name)
-
-	#----------------------------------------
-	# commit all files
-	#----------------------------------------
-	git_add_and_commit('first commit')
-
-	#----------------------------------------
-	# push to github and heroku
-	#----------------------------------------
-	if args.push:
-		os.system('git push origin master')
-		os.system('git push heroku master; heroku open')
-
-	#----------------------------------------
-	# print success message
-	#----------------------------------------
-	print "\nYour app has been created!"
 
 if __name__ == "__main__":
 	main()
